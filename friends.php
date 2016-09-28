1b07eadfcc3b1a3f67811682ebbb063cf12c497b
ZodiMac <zodimac@outlook.com>
2016-09-28 23:01:55 +0800

    a sample page

--- friends.php
+++ friends.php
@@ -16,10 +16,63 @@
 	</div>
 	<div class="friends">
 		<div class="friends_box">
-			<img src="sources/img/avatar/nekotora.jpg">
+			<a href="http://flag.moe"><img src="sources/img/avatar/NekoTora.jpg"></a>
 			<h3>NekoTora</h3>
 			<div class="friends_box_invisible">
-				<p>蘑菇神社的守护者。</p>
+				<p>前端/PHP/摄影/设计/赛艇/二次元</p>
+			</div>
+			<div class="sidebar">
+				<ul>
+					<li><a>前端</a></li>
+					<li><a>PHP</a></li>
+					<li><a>射影</a></li>
+					<li><a>女装</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="friends_box">
+			<a href="https://www.sylingd.com/"><img src="sources/img/avatar/sylingd.jpg"></a>
+			<h3>泷涯零点</h3>
+			<div class="friends_box_invisible">
+				<p>前端/PHP/音乐/赛艇</p>
+			</div>
+			<div class="sidebar">
+				<ul>
+					<li><a>前端</a></li>
+					<li><a>PHP</a></li>
+					<li><a>射影</a></li>
+					<li><a>女装</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="friends_box">
+			<a href="http://wocao.moe"><img src="sources/img/avatar/cs538138.jpg"></a>
+			<h3>cs538138</h3>
+			<div class="friends_box_invisible">
+				<p>电子/运维/飙车</p>
+			</div>
+			<div class="sidebar">
+				<ul>
+					<li><a>前端</a></li>
+					<li><a>PHP</a></li>
+					<li><a>射影</a></li>
+					<li><a>女装</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="friends_box">
+			<a href="http://jipai.flag.moe"><img src="sources/img/avatar.jpg"></a>
+			<h3>鸡排</h3>
+			<div class="friends_box_invisible">
+				<p>前端</p>
+			</div>
+			<div class="sidebar">
+				<ul>
+					<li><a>前端</a></li>
+					<li><a>PHP</a></li>
+					<li><a>射影</a></li>
+					<li><a>女装</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
