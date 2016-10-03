f6a0f2a045a93d2fe6e0af5b28b9642ff217dc26
ZodiMac <zodimac@outlook.com>
2016-10-03 23:49:26 +0800

    Die!Die!Die!

--- friends.php
+++ friends.php
@@ -11,69 +11,44 @@
 </head>
 <body>
 	<?php include'part/nav.php';?>
-	<div class="title">
-		<h2>Web Developer</h2>
-	</div>
+
 	<div class="friends">
+		<div class="web-developer">
+		<div class="title">
+			<h2>Web Developer</h2>
+		</div>
 		<div class="friends_box">
-			<a href="http://flag.moe"><img src="sources/img/avatar/NekoTora.jpg"></a>
+			<a href="http://flag.moe"><img src="sources/img/avatar/NekoTora.jpg">
 			<h3>NekoTora</h3>
 			<div class="friends_box_invisible">
 				<p>前端/PHP/摄影/设计/赛艇/二次元</p>
 			</div>
-			<div class="sidebar">
-				<ul>
-					<li><a>前端</a></li>
-					<li><a>PHP</a></li>
-					<li><a>射影</a></li>
-					<li><a>女装</a></li>
-				</ul>
-			</div>
+			</a>
 		</div>
 		<div class="friends_box">
-			<a href="https://www.sylingd.com/"><img src="sources/img/avatar/sylingd.jpg"></a>
+			<a href="https://www.sylingd.com/"><img src="sources/img/avatar/sylingd.jpg">
 			<h3>泷涯零点</h3>
 			<div class="friends_box_invisible">
 				<p>前端/PHP/音乐/赛艇</p>
 			</div>
-			<div class="sidebar">
-				<ul>
-					<li><a>前端</a></li>
-					<li><a>PHP</a></li>
-					<li><a>射影</a></li>
-					<li><a>女装</a></li>
-				</ul>
-			</div>
+			</a>
 		</div>
 		<div class="friends_box">
-			<a href="http://wocao.moe"><img src="sources/img/avatar/cs538138.jpg"></a>
+			<a href="http://wocao.moe"><img src="sources/img/avatar/cs538138.jpg">
 			<h3>cs538138</h3>
 			<div class="friends_box_invisible">
 				<p>电子/运维/飙车</p>
 			</div>
-			<div class="sidebar">
-				<ul>
-					<li><a>前端</a></li>
-					<li><a>PHP</a></li>
-					<li><a>射影</a></li>
-					<li><a>女装</a></li>
-				</ul>
-			</div>
+			</a>
 		</div>
 		<div class="friends_box">
-			<a href="http://jipai.flag.moe"><img src="sources/img/avatar.jpg"></a>
+			<a href="http://jipai.flag.moe"><img src="sources/img/avatar.jpg">
 			<h3>鸡排</h3>
 			<div class="friends_box_invisible">
 				<p>前端</p>
 			</div>
-			<div class="sidebar">
-				<ul>
-					<li><a>前端</a></li>
-					<li><a>PHP</a></li>
-					<li><a>射影</a></li>
-					<li><a>女装</a></li>
-				</ul>
-			</div>
+			</a>
+		</div>
 		</div>
 	</div>
 	<div class="motto"><p>相逢有时·后会无期</p></div>	
