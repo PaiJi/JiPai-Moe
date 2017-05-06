7dd2e4385445000b2b2a5238bd0963994ad1a4f3
imac Zod <imac Zod>
2017-05-06 09:46:15 +0800

    Version beta2.0.0 BEGIN.

---
+++ v2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JiPai|Store</title>
+</head>
+<body>
+    <p>迷失的人迷失了，相逢的人会再相逢。——《挪威的森林》</p>
+    <p>全新JiPai|Store正在咕咕咕上线中</p>
+</body>
+</html>
