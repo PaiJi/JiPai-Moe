326e57a4d7f34e431fb7b8ed5eddbec4ff964ef5
imac Zod <imac Zod>
2017-05-07 21:38:37 +0800

    A welcome page for now.

--- v2/index.php
+++ v2/index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="sources/css/index.css">
+    <script src="//cdn.bootcss.com/jquery/3.1.0/jquery.min.js"></script>
     <title>JiPai|Store</title>
 </head>
 <body>
-    <p>迷失的人迷失了，相逢的人会再相逢。——《挪威的森林》</p>
-    <p>全新JiPai|Store正在咕咕咕上线中</p>
+    <div class="content">
+    <p id="motto">迷失的人迷失了，相逢的人会再相逢。<br></p>
+    <p id="motto2">全新<span>&nbspJiPai|Store&nbsp</span>咕咕咕上线中</p>
+    </div>
 </body>
 </html>
