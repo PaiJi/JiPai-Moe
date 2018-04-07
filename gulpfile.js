d6f1dd95934bee343fc287c2b88eca154214714a
PaiJi <github@jipai.moe>
2018-04-07 13:54:59 +0800

    初步可用的gulp打包配置.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,45 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    browserSync = require("browser-sync").create();
 gulp.task('default',function(){
 
 });
 gulp.task('console',function(){
-    console.log('what will happen?');
+    console.log('Bring the world light.');
 });
+gulp.task('sass', function () {
+    gulp.src('./sources/css/sass/*.scss')
+        .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./sources/css/stylesheets/'));
+});
+gulp.task('serve', function (done) {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            serveStaticOptions: {
+                extensions: ['html'] // pretty urls
+            }
+        },
+
+    });
+    done();
+});
+gulp.task('dev', ['sass', 'serve'], function (cb) {
+    //gulp.watch('./sources/**/*.js', ['scripts']);
+    //gulp.watch('./**/*.html', ['html']);
+    gulp.watch('./sources/css/sass/*.scss', ['sass']);
+
+    gulp.watch('./*.html', ['reload']);
+    gulp.watch('./sources/css/sass/*.scss', ['reload']);
+    gulp.task('console');
+});
+
+gulp.task('reload', function (done) {
+    setTimeout(function () {
+        browserSync.reload();
+        done();
+    }, 300)
+});
+gulp.task('build', ['scripts', 'css', 'html', 'static'])
