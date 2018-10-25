80be2ce06f9a3a3c17eb3d6e4a2651d975dcfdf7
PaiJi <github@jipai.moe>
2018-10-25 15:45:16 +0800

    gulp 增加lab和bgm页面的编译信息。

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,7 @@ var gulp = require('gulp'),
     del = require('del'),
     sass = require('gulp-sass'),
     browserSync = require("browser-sync").create();
-gulp.task('default',function(){
 
-});
-gulp.task('console',function(){
-    console.log('Bring the world light.');
-});
 gulp.task('sass', function () {
     gulp.src('./sources/css/sass/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
@@ -18,6 +13,11 @@ gulp.task('sass-lab', function () {
     gulp.src('./lab/sources/css/sass/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./lab/sources/css/stylesheets/'));
+});
+gulp.task('sass-bgm', function () {
+    gulp.src('./bgm/sources/css/sass/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./bgm/sources/css/stylesheets/'));
 });
 gulp.task('serve', function (done) {
     browserSync.init({
@@ -34,13 +34,18 @@ gulp.task('serve', function (done) {
 gulp.task('dev', ['sass', 'serve'], function (cb) {
     //gulp.watch('./sources/**/*.js', ['scripts']);
     //gulp.watch('./**/*.html', ['html']);
+    //下面是index用的配置
     gulp.watch('./sources/css/sass/*.scss', ['sass']);
-    gulp.watch('./lab/sources/css/sass/*.scss', ['sass-lab']);
     gulp.watch('./*.html', ['reload']);
-    gulp.watch('./lab/*.html', ['reload']);
     gulp.watch('./sources/css/sass/*.scss', ['reload']);
+    //下面是lab用的配置 
+    gulp.watch('./lab/sources/css/sass/*.scss', ['sass-lab']);
+    gulp.watch('./lab/*.html', ['reload']);
     gulp.watch('./lab/sources/css/sass/*.scss', ['reload']);
-    gulp.task('console');
+    //下面是bgm用的配置
+    gulp.watch('./bgm/sources/css/sass/*.scss', ['sass-bgm']);
+    gulp.watch('./bgm/*.html', ['reload']);
+    gulp.watch('./bgm/sources/css/sass/*.scss', ['reload']);
 });
 
 gulp.task('reload', function (done) {
