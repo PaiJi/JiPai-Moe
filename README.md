703895e644c62503f8efe31bdd214b1fd3860e67
PaiJi <github@jipai.moe>
2021-10-23 22:30:38 +0800

    index swiper import data from json now

---
+++ README.md
@@ -0,0 +1,1 @@
+JiPai.Moe site repo
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,29 @@
 import React from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay } from "swiper"
+import swiperImagesJson from "../../content/swiperImages.json"
 
 export default function Home() {
   return (
-        <main className="my-4">
-          <section
-            className="shadow my-4 rounded-md overflow-hidden"
-          >
-            <Swiper className="rounded-md overflow-hidden">
-              <SwiperSlide className="">
-                <img src="https://qcloud-cdn-static.lonepixel.cn/blog/2018_12_31_kedama.webp" />
-              </SwiperSlide>
-            </Swiper>
-          </section>
+    <main className="my-4">
+      <section className="index-swiper shadow my-4 rounded-md overflow-hidden">
+        <Swiper modules={[Autoplay]} autoplay={{delay:5000}} className="rounded-md overflow-hidden h-full">
+          {swiperImagesJson.index.map(image => (
+            <SwiperSlide key={image.name}>
+              <div className="h-full w-full bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${image.link})` }}></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
 
-          <section className="bg-white shadow py-8 px-4 my-4 rounded-md">
-            <span className="text-3xl font-bold block text-center text-dark-666 tracking-widest">
-              生命之光终将照耀地平线。
-            </span>
-            <label className="block text-right text-gray-102">
-              来自·毛玉線圈物語Minecraft
-            </label>
-          </section>
-        </main>
+      <section className="bg-white shadow py-8 px-4 my-4 rounded-md">
+        <span className="text-3xl font-bold block text-center text-dark-666 tracking-widest">
+          生命之光终将照耀地平线。
+        </span>
+        <label className="block text-right text-gray-102">
+          来自·毛玉線圈物語Minecraft
+        </label>
+      </section>
+    </main>
   )
 }
